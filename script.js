4b083145e7634cb519af862ea1b743ccacc7906f
divra12 <101518898+divra12@users.noreply.github.com>
2024-05-10 21:48:41 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,32 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var video = document.getElementById('gameVideo');
+    video.removeAttribute('controls'); // Entfernen der Steuerelemente
+
+    // Starten des Videos bei Benutzerinteraktion
+    var gameContainer = document.querySelector('.game-container');
+    var overlay = document.querySelector('.overlay');
+    var isVideoPlaying = false;
+
+    gameContainer.addEventListener('mouseenter', function () {
+        if (!isVideoPlaying) {
+            overlay.style.display = 'flex';
+            video.play();
+            isVideoPlaying = true;
+        }
+    });
+
+    gameContainer.addEventListener('mouseleave', function () {
+        overlay.style.display = 'none';
+        video.pause();
+        isVideoPlaying = false;
+    });
+});
+
+// Link zu passender Webseite
+var bildElement = document.getElementById('Train-Sim');
+
+// Füge einen Event-Listener hinzu, der auf das Klicken des Bildes reagiert
+bildElement.addEventListener('click', function() {
+    // Öffne eine neue Seite 
+    window.open('Train-Sim', '_blank');
+});
